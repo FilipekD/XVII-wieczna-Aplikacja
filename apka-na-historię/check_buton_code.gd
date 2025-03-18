28add59bc36d2b3e71c72152301a66c43335fdd5
xDx5 <papajowapapaja4321@gmail.com>
2025-03-18 20:24:53 +0100

    Check box działa

--- apka-na-historię/check_buton_code.gd
+++
@@ -1,6 +0,0 @@
-extends CheckBox
-
-func _ready():
-	# Adjust checkmark size and spacing
-	add_theme_constant_override("check_v_offset", 10)  # Moves checkmark down
-	add_theme_constant_override("hseparation", 15)  # Adjusts spacing
